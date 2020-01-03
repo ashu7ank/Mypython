a1e57fc1bc3900a1cece42473654c4e6b761a2f0
Kumar <830334@cognizant.com>
2020-01-03 14:08:04 +0530

    first commit

---
+++ Ashu/Ashu.py
@@ -0,0 +1,20 @@
+"""dob=input("enter your dob: ")
+age=2020 - int(dob)
+print(" age= ",age)
+print(type (dob))"""
+
+
+"""wip=int (input("enter weight in pounds: "))
+wik=wip*0.45
+print("weight in kg is :",wik,"kg")
+my_str='''Hello Ram 
+happy new year
+This is cts.
+Swaagat nahi karoge hamara'''
+print(my_str)"""
+
+fname=input("Enter first name:")
+lname=input("Enter last name:")
+#msg1=(fname + '[' + lname + '] is a system admin')
+msg2=f' {lname} [{lname}] is a system admin'
+print(msg2)
