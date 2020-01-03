c96dd2ea80a9cf38e4cdf4db00d2a4a690918db1
Kumar <830334@cognizant.com>
2020-01-03 15:56:37 +0530

    New update

--- Ashu/Ashu.py
+++ Ashu/Ashu.py
@@ -1,20 +1,34 @@
 """dob=input("enter your dob: ")
 age=2020 - int(dob)
 print(" age= ",age)
-print(type (dob))"""
+print(type (dob))
 
 
-"""wip=int (input("enter weight in pounds: "))
+wip=int (input("enter weight in pounds: "))
 wik=wip*0.45
 print("weight in kg is :",wik,"kg")
 my_str='''Hello Ram 
 happy new year
 This is cts.
 Swaagat nahi karoge hamara'''
-print(my_str)"""
+print(my_str)
 
 fname=input("Enter first name:")
 lname=input("Enter last name:")
 #msg1=(fname + '[' + lname + '] is a system admin')
 msg2=f' {lname} [{lname}] is a system admin'
 print(msg2)
+
+is_rainy=True
+if is_rainy:
+    print ("It is a rainy day")
+print("Enjoy your day")"""
+
+num=int(input("enter a no."))
+if num<0:
+    print("Negative Integer")
+elif num==0:
+    print("zero")
+else:
+    print("Positive value")
+
