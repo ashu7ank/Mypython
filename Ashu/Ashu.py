a06da2ac6ccff670ce397e142d246c4789e0af55
Kumar <830334@cognizant.com>
2020-01-03 15:04:38 +0530

    added some info

--- Ashu/Ashu.py
+++ Ashu/Ashu.py
@@ -13,8 +13,23 @@ This is cts.
 Swaagat nahi karoge hamara'''
 print(my_str)"""
 
-fname=input("Enter first name:")
+'''fname=input("Enter first name:")
 lname=input("Enter last name:")
 #msg1=(fname + '[' + lname + '] is a system admin')
 msg2=f' {lname} [{lname}] is a system admin'
-print(msg2)
+print(msg2)'''
+
+my_text="PYTHON for beginners"
+print(len(my_text))
+print(my_text.upper())
+#print(my_text.find('O'))
+print(my_text.replace('O','P'))
+print('PYTHON' in my_text)
+cube=[1,8,27,64,125]
+print (cube)
+cube.append(216)
+print(cube)
+cube.append(346)
+print(cube)
+cube[3]=343
+print(cube)
